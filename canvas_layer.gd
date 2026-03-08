0fc62f944330f6fedda823a1b564541020add2ac
1004genre <145152056+1004genre@users.noreply.github.com>
2026-03-08 05:11:22 -0700

    Add files via upload
    every isha pp file ever

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -1,25 +1,87 @@
 extends CanvasLayer
 
-@onready var textbox_container = $Textboxalicious
-@onready var start_symbol = $Textboxalicious/Panel/MarginContainer/Startt
-@onready var end_symbol = $Textboxalicious/Panel/MarginContainer/End
+signal dialogue_finished
+
+enum State {
+	READY,
+	READING,
+	FINISHED
+}
+
+const CHAR_RATE = 0.075
+
 @onready var label = $Textboxalicious/Panel/MarginContainer/Panel/Label
+@onready var bg = $Textboxalicious/Panel   # panel holding the texture
+
+var tween: Tween
+var state: State
 
 func _ready():
-	hide_textbox()
-	add_text("you're playing period poverty!")
+	_hide()
+	state = State.READY
+	
+	randomize()
+	_hide()
+	state = State.READY
 
-func hide_textbox():
-	start_symbol.text = ""
-	end_symbol.text = ""
+
+func _process(_delta):
+
+	if Input.is_action_just_pressed("ui_select"):
+
+		match state:
+
+			State.READY:
+				_display_dialogue("welcome to period poverty!")
+
+			State.READING:
+				_finish()
+
+			State.FINISHED:
+				_hide()
+				state = State.READY
+
+				# second popup happens here
+				show_pad_result()
+
+
+func _hide():
 	label.text = ""
-	textbox_container.hide()
-	
-func start_textbox():
-	start_symbol.text = "*"
-	textbox_container.show()
+	label.visible_ratio = 0
+	bg.visible = false
 
-func add_text(next_text):
-	label.text = next_text
-	start_textbox()
+
+func _show():
+	bg.visible = true
+	tween = create_tween()
+	tween.finished.connect(_finish)
+
+
+func _finish():
+	if tween:
+		tween.kill()
+
+	label.visible_ratio = 1
+	state = State.FINISHED
+
+
+func _display_dialogue(lines):
+
+	label.text = lines
+	label.visible_ratio = 0
+
+	_show()
+
+	tween.tween_property(label, "visible_ratio", 1.0, len(lines) * CHAR_RATE)
+
+	state = State.READING
 	
+func show_pad_result():
+
+	var chance = randf()
+
+	if chance <= 0.3:
+		_display_dialogue("you found a pad!")
+	else:
+		_display_dialogue("there are no pads in this restroom :(")
+		
